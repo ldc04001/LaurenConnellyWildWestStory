c6da70e13c7625edf329d87fcb1648dd7c5850b1
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-06 22:53:34 +0000

    background and first question

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ print("One rainy day, you decide to hop in your time machine to go on an adventu
 print("You decide to head to town to see what life in the wild west offers!")
 
 # Display the first multiple choice question to choose if you will talk to the  bartender, the sheriff, or the outlaw
+print("You ride your horse into town and see a few places you could stop in to get some information:")
+print(" A. The local saloon, where the bartender's waiting to serve you")
+print(" B. The sheriff is standing outside the local jail")
+print(" C. An outlaw, is loading up his six-shooter in the shadows")
 
 # Have the reader type the letter of who they want to talk to
-
+firstChoice = input("Who will you talk to? Please selection option A through C: ")
 # If they picked the bartender, ask the reader a multiple choice question on what drink the want
 
 # Have the reader type the letter of what drink they want

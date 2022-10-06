425a8b01d5f5285bed601f963a90e2beb599ca57
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-06 23:54:04 +0000

    cleaned up a few typos and formatting

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ print("You only have two days to explore so you decide to head to town to see wh
 print( )
 
 # Display the first multiple choice question to choose if you will talk to the  bartender, the sheriff, or the outlaw
-print("You ride your horse into town and see a few places you could stop in to get some information:")
+print("You ride your horse into town and spot a few things you've seen in the movies that you'd like to check out:")
 print(" A. The local saloon, where the bartender's waiting to serve you")
 print(" B. The sheriff is standing outside the local jail")
-print(" C. An outlaw, is loading up his six-shooter in the shadows")
+print(" C. An outlaw is loading up his six-shooter in the shadows")
 
 # Have the reader type the letter of who they want to talk to
 firstChoice = input("Who will you talk to? Please selection option A through C: ")
@@ -21,18 +21,18 @@ print( )
 
 # If they picked the bartender, ask the reader a multiple choice question on what drink the want
 if(firstChoice == "A"): 
-  print("You walk in into the saloon, and the bartender asks you what you want to drink")
+  print("You walk in into the saloon, and the bartender asks you what you want to drink. What do you order? ")
   print(" A. boilermaker")
   print(" B. moonshine")
   print(" C. a beer")
  
   # Have the reader type the letter of what drink they want 
-  drinkChoice = input("What do you order? Please enter a letter A through C: ")
+  drinkChoice = input("Please enter a letter A through C: ")
   print( )
 
   #If the reader picked the moonshine, display the message that they'll get in a fight and go to jail
   if(drinkChoice == "B"):
-    print("You get into fight, then the sheriff hauls you off to jail")
+    print("Boy that moonshine was strong. You end up picking a fight with one of the regulars, then the sheriff hauls you off to jail")
     print("~ The End~")
     print( )
 
@@ -50,12 +50,12 @@ if(firstChoice == "B"):
   print( )
   # If they entered the correct answer, display message
   if(horseAnswer == 15):
-    print("The sheriff says, 'Boy you're pretty, would you like to be my deputy?' You gladly accept the position.")
+    print("The sheriff says, 'Boy you're pretty smart, would you like to be my deputy?' You gladly accept the position and help the sheriff clean up the town.")
     print("~ The End ~")
 
   # If they answer incorrectly, display message telling you to get lost
   else:
-    print("The sheriff shakes his head and tells you good luck")
+    print("The sheriff shakes his head and says 'I think you've been out in the sun too long'. Looks like law enforcement career is not in the cards for you.")
     print("~ The End ~")
 
 # If they picked the outlaw, ask the reader if he want to join the outlaws posse

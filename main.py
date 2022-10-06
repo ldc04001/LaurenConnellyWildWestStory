c3554befbe3b91a8c20573d9d8e653b3ca1c0ef9
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-06 22:45:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Display the Tttle of the story
+print("~ Tales of the Wild West")
+print("A Work of Fiction")
+
+# Give the reader the story background
+print("One rainy day, you decide to hop in your time machine to go on an adventure. Your time machine transports you to the 1880s, and it becomes clear to you that you've ended up in the Wild West.")
+
+print("You decide to head to town to see what life in the wild west offers!")
+
+# Display the first multiple choice question to choose if you will talk to the  bartender, the sheriff, or the outlaw
+
+# Have the reader type the letter of who they want to talk to
+
+# If they picked the bartender, ask the reader a multiple choice question on what drink the want
+
+# Have the reader type the letter of what drink they want
+
+# If the reader picked the boilermaker, display the message that they will get into a fight
+
+#If the reader picked the moonshine, display the message that they'll pass out
+
+# If the reader picked the beer, display the message that they'll be on their way after
+
+# If they picked the sheriff, ask the reader to answer the sheriff's question of how many days you're staying in town
